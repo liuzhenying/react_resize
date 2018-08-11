e14372709dbeb7954887c676327b6edf992bab1e
刘珍莹 <1803275951@qq.com>
2018-08-11 16:09:14 +0800

    feat: 初始化工程

---
+++ examples/src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'react-component-template/style/component.css';// eslint-disable-line
+import Block from 'react-component-template/src'; // eslint-disable-line
+
+ReactDOM.render(
+  <div className="component-template">
+    <Block />
+  </div>,
+  document.getElementById('example'),
+);
